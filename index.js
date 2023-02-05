df379637ce5ed22cc490c0474911baa7ac29e567
MOOOGAMES3 <96015772+MOOOGAMES3@users.noreply.github.com>
2023-02-05 16:50:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+// window.alert("Hello From JS File")
+
+
+// التكليف 02
+// التكليف 02
+
+// document.querySelector("h1").style.color=" blue";
+// document.querySelector("h1").style.fontSize="80px";
+// document.querySelector("h1").style.fontWeight="bold";
+
+// التكليف 02
+// التكليف 02
+
+
+
+
+// التكليف 03
+// التكليف 03
+
+// console.log(
+//   "%cJS %cCSS %cTEST",
+//   "color:red; font-size:40px",
+//   "color:green; font-size:40px; font-weight: bold;",
+//   "color:blue; font-size:40px"
+// );
+
+// التكليف 03
+// التكليف 03
+
+
+// التكليف 04
+// التكليف 04
+
+// console.group("JS-1");
+// console.log("JS-1JS-1JS-1JS-1JS-1JS-1JS-1JS-1JS-1JS-1JS-1");
+// console.group("JS-2");
+// console.log("JS-2JS-2JS-2JS-2JS-2JS-2JS-2JS-2");
+// console.group("JS-3");
+// console.log("JS-3JS-3JS-3JS-3JS-3JS-3JS-3JS-3");
+
+// التكليف 04
+// التكليف 04
+
+
+
+//التكليف 05
+//التكليف 05
+
+// console.table (["HTML" , "CSS" , "JS"])
+
+//التكليف 05
+//التكليف 05
